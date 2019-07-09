edea02799077e2560a828b82cf98e5f4111d920b
wasuken <wevorence@gmail.com>
2019-07-10 01:37:05 +0900

    commit.

--- src/act11.hs
+++ src/act11.hs
@@ -1,5 +1,10 @@
+import Control.Applicative
+
 quicksort :: [Int] -> [Int]
 quicksort [] = []
 quicksort (x:xs) = quicksort lt ++ [x] ++ quicksort gt
   where lt = filter (< x) xs
         gt = filter (>= x) xs
+
+seqA :: (Applicative f) => [f a] -> f [a]
+seqA = foldr (liftA2 (:)) (pure [])
